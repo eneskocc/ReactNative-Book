0f01e3b6d630df158f120f500fb4149e6cdddae6
Enes koç <32553624+eneskocc@users.noreply.github.com>
2022-03-07 13:55:28 +0300

    Update Book.js

--- Book-api/models/Book.js
+++ Book-api/models/Book.js
@@ -2,19 +2,18 @@ const mongoose=require('mongoose');
 const Movie=require('../models/Book');
 const Schema=mongoose.Schema;
 const BookSchema=mongoose.Schema({
-    director_id:Schema.Types.ObjectId,
-    title:{
+    book_id:Schema.Types.ObjectId,
+    name:{
         type:String,
         required:true
     },
     category:String,
-    country:String,
-    year:Number,
-    imdb_score:Number,
-    date:{
-        type:Date,
-        default:Date.now
-    }
+    content:String,
+    number:Number,
+    img:Number,
+    price:Number,
+    discount:Number,
+    newPrice:Number,
 });
 
 module.exports=mongoose.model('movie',MovieSchema);
